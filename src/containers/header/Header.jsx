e252292ac16c614a0a54a189c0d538931803bc4c
hanurakesh <hanurakesh11@gmail.com>
2022-01-21 18:49:44 +0530

    header updated

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,8 +6,8 @@ import './header.css';
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <h1 className="gradient__text">Let&apos;s Build Something amazing </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.  necessitatibus harum. Mollitia a voluptatum consequatur delectus sed quis cumque.</p>
 
       <div className="gpt3__header-content__input">
         <input type="email" placeholder="Your Email Address" />
